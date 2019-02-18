004cecae5c08444573dec0cf5d2564c2522676d2
ketantawade <ketan.tawade@nazara.com>
2019-02-18 17:58:33 +0530

    unblockplayer

--- app/routes/master/playerRoute.js
+++ app/routes/master/playerRoute.js
@@ -3,5 +3,6 @@ var playerController = require('../../controller/master/playerController');
 
 appRoutes.post('/player/refundplayer', playerController.refundPlayer);
 appRoutes.post('/player/getpendingrefund', playerController.getPending); appRoutes.post('/player/refundapproved', playerController.refundApproved);
+appRoutes.post('/player/unblockplayer', playerController.unblockPlayer);
 
 module.exports = appRoutes;
