24e6accf1fdf97d5449acee12c658c8d83a2fa96
shrinath.rao <shrinath.rao@nazara.local>
2019-05-30 12:27:39 +0530

    new api

--- app/controller/scratchCard/scratchBannerController.js
+++ app/controller/scratchCard/scratchBannerController.js
@@ -27,21 +27,8 @@ module.exports = {
     add: async function (req, res) {
 
         let rules = {
-            // "banner_id": 'required|numeric',
             "banner_name": 'required',
-            // "description": 'required',
-            // "image_url": 'required',
-            // "banner_type": 'required',
-            // "app_id": 'required|numeric',
-            // "contest_id": 'required|numeric',
-            // "click_url": 'required',
-            // "width": 'required|numeric',
-            // "height": 'required',
-            // "banner_priority": 'required',
             "status": 'required|in:ACTIVE,DEACTIVE,PENDING',
-            // "created_date": 'required',
-            // "page": 'required',
-            // "channel": 'required',
         };
 
         let obj = req.body
@@ -70,18 +57,18 @@ module.exports = {
             if (!_banner_id) {
 
                 _query = {
-                    text: "INSERT INTO tbl_scratch_banner_master(banner_name,description,click_url,banner_priority,status,created_date) VALUES ($1,$2,$3,$4,$5,$6) RETURNING *",
+                    text: "INSERT INTO tbl_scratch_banner_master(banner_name,description,click_url,banner_priority,status,created_date) VALUES ($1,$2,$3,$4,$5,now()) RETURNING *",
                     values: [
-                        _banner_name, _description, _click_url, _banner_priority, _status, _created_date
+                        _banner_name, _description, _click_url, _banner_priority, _status
                     ]
                 }
             }
             else {
 
                 _query = {
-                    text: "UPDATE tbl_scratch_banner_master SET banner_id=$1,banner_name=$2,description=$3,click_url=$4,banner_priority=$5,status=$6,created_date=$7 RETURNING *",
+                    text: "UPDATE tbl_scratch_banner_master SET banner_name=$1,description=$2,click_url=$3,banner_priority=$4,status=$5,created_date=$6 WHERE banner_id=$7  RETURNING *",
                     values: [
-                        _banner_id, _banner_name, _description, _click_url, _banner_priority, _status, _created_date
+                        _banner_name, _description, _click_url, _banner_priority, _status, _created_date
                     ]
                 }
 
---
+++ app/controller/scratchCard/scratchBannerDetailsController.js
@@ -0,0 +1,121 @@
+const pgConnection = require('../../model/pgConnection');
+
+const services = require('../../service/service');
+
+const customMsgType = "MASTER_MESSAGE";
+const customMsgTypeCM = "COMMON_MESSAGE";
+
+module.exports = {
+
+    getAll: async function (req, res) {
+
+        let _selectQuery = "SELECT * FROM tbl_scratch_campaign_banner"
+        try {
+            let dbResult = await pgConnection.executeQuery('rmg_dev_db', _selectQuery)
+
+            if (dbResult && dbResult.length > 0) {
+                services.sendResponse.sendWithCode(req, res, dbResult, customMsgType, "GET_SUCCESS");
+            }
+            else
+                services.sendResponse.sendWithCode(req, res, dbResult, customMsgType, "GET_FAILED");
+        }
+        catch (error) {
+            services.sendResponse.sendWithCode(req, res, error, customMsgTypeCM, "DB_ERROR");
+        }
+    },
+
+    add: async function (req, res) {
+
+        let rules = {
+            "camp_id": 'required|numeric',
+            "banner_id": 'required|numeric',
+            "status": 'required|in:ACTIVE,DEACTIVE,PENDING',
+        };
+
+        let obj = req.body
+        // deleting null value from req.body
+        Object.keys(obj).forEach(k => (obj[k] === 'null') && delete obj[k]);
+
+        let validation = new services.validator(req.body, rules);
+
+        if (validation.passes()) {
+            
+            let _id = req.body.id ? req.body.id : null;
+            let _camp_id = req.body.camp_id ? req.body.camp_id : null;
+            let _banner_id = req.body.banner_id ? req.body.banner_id : null;
+            let _status = req.body.status ? req.body.status : null;
+            let _created_date = req.body.created_date ? req.body.created_date : null;
+            let _created_by = req.body.userid ? req.body.userid : null;
+            let _updated_by = req.body.userid ? req.body.userid : null;
+
+            let _query;
+            let errMsgType = _id ? 'UPDATE_FAILED' : 'ADD_FAILED'
+            let successMsgType = _id ? 'UPDATE_SUCCESS' : 'ADD_SUCCESS'
+
+            if (!_id) {
+
+                _query = {
+                    text: "INSERT INTO tbl_scratch_campaign_banner(camp_id,banner_id,status,created_date) VALUES ($1,$2,$3,$4) RETURNING *",
+                    values: [
+                        _camp_id, _banner_id, _status, _created_date
+                    ]
+                }
+            }
+            else {
+
+                _query = {
+                    text: "UPDATE tbl_scratch_campaign_banner SET id=$1,camp_id=$2,banner_id=$3,status=$4,created_date=$5 RETURNING *",
+                    values: [
+                        _id, _camp_id, _banner_id, _status, _created_date
+                    ]
+                }
+
+            }
+        }
+
+        else {
+            services.sendResponse.sendWithCode(req, res, validation.errors.errors, customMsgTypeCM, "VALIDATION_FAILED");
+
+        }
+    },
+
+    search: async function (req, res) {
+
+        let _banner_id = req.body.banner_id ? req.body.banner_id : null;
+        let _camp_id = req.body._camp_id ? req.body._camp_id : null;
+        let _status = req.body.status ? req.body.status : null;
+        //let _banner_type = req.body.banner_type ? req.body.banner_type : null;
+        let _orderBy = req.body.orderBy ? req.body.orderBy : 'created_date';
+
+        let _selectQuery = 'SELECT * FROM tbl_scratch_campaign_banner WHERE  1=1'
+
+        if (_banner_id) {
+            _selectQuery += " AND banner_id = " + _banner_id
+        }
+
+        if (_camp_id) {
+            _selectQuery += " AND camp_id = " + _camp_id
+        }
+
+        if (_status) {
+            _selectQuery += " AND status = '" + _status + "'"
+        }
+
+        _selectQuery += " order by " + _orderBy;
+
+        try {
+            let dbResult = await pgConnection.executeQuery('rmg_dev_db', _selectQuery)
+
+            if (dbResult && dbResult.length > 0) {
+                services.sendResponse.sendWithCode(req, res, dbResult, customMsgType, "GET_SUCCESS");
+            } else {
+                services.sendResponse.sendWithCode(req, res, dbResult, customMsgType, "GET_FAILED");
+            }
+        }
+        catch (error) {
+            services.sendResponse.sendWithCode(req, res, error, customMsgTypeCM, "DB_ERROR");
+        }
+
+    },
+
+}
